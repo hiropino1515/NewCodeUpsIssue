25fd5c791ebd4826a8a8d48a39b92077b30394c8
hiropino1515 <hiropino1515@gmail.com>
2023-09-07 06:40:18 +0900

    6 commit

--- src/js/script.js
+++ src/js/script.js
@@ -3,3 +3,41 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
 
 
 });
+// ドロワーメニュー
+$(".js-hamburger").click(function () {
+  if ($(".js-hamburger").hasClass("is-active")) {
+    $(".js-hamburger").removeClass('is-active');
+
+    $(".js-sp-nav").fadeOut(300);
+  } else {
+    $(".js-hamburger").addClass('is-active');
+    $(".js-sp-nav").fadeIn(300);
+  }
+});
+
+var swiper = new Swiper(".js-campaign-swiper", {
+  // pagination: {
+  //   el: ".js-works-paginations",
+  // },
+  loop: true,
+  clickable: true,
+  autoplay: {
+    delay: 3000,
+  },
+
+  slidesPerView: 1.25,
+  spaceBetween: 24,
+
+  breakpoints: {
+    // ウィンドウサイズが767px以下
+    767: {
+      slidesPerView: 1.25,
+      spaceBetween: 24,
+    },
+
+    768: {
+      slidesPerView: 3.5,
+      spaceBetween: 41,
+    },
+  },
+});
