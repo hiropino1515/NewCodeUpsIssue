9379a1ecb83a3a0da0d8afad99fc319b1175efb2
hiropino1515 <hiropino1515@gmail.com>
2023-09-11 23:33:02 +0900

    9 commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,47 +1,72 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
+  let topBtn = $('.to-top');
+  topBtn.hide();
 
-});
-// ドロワーメニュー
-$(".js-hamburger").click(function () {
-  if ($(".js-hamburger").hasClass("is-active")) {
-    $(".js-hamburger").removeClass('is-active');
+  // ボタンの表示設定
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 70) {
+      // 指定px以上のスクロールでボタンを表示
+      topBtn.fadeIn();
+    } else {
+      // 画面が指定pxより上ならボタンを非表示
+      topBtn.fadeOut();
+    }
+  });
 
-    $(".js-sp-nav").fadeOut(300);
-  } else {
-    $(".js-hamburger").addClass('is-active');
-    $(".js-sp-nav").fadeIn(300);
-  }
-});
+  // ドロワーメニュー
+  $(".js-hamburger").click(function () {
+    if ($(".js-hamburger").hasClass("is-active")) {
+      $(".js-hamburger").removeClass('is-active');
 
-var swiper = new Swiper(".js-campaign-swiper", {
-  // pagination: {
-  //   el: ".js-works-paginations",
-  // },
-  loop: true,
-  clickable: true,
-  autoplay: {
-    delay: 3000,
-  },
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  slidesPerView: 1.25,
-  spaceBetween: 24,
+      $(".js-sp-nav").fadeOut(300);
+    } else {
+      $(".js-hamburger").addClass('is-active');
+      $(".js-sp-nav").fadeIn(300);
+    }
+  });
 
-  breakpoints: {
-    // ウィンドウサイズが767px以下
-    767: {
-      slidesPerView: 1.25,
-      spaceBetween: 24,
+  var swiper = new Swiper(".js-mv-swiper", {
+    // pagination: {
+    //   el: ".js-works-paginations",
+    // },
+    loop: true,
+    clickable: true,
+    autoplay: {
+      delay: 3000,
     },
+  });
 
-    768: {
-      slidesPerView: 3.5,
-      spaceBetween: 41,
+  var swiper = new Swiper(".js-campaign-swiper", {
+    // pagination: {
+    //   el: ".js-works-paginations",
+    // },
+    loop: true,
+    clickable: true,
+    autoplay: {
+      delay: 3000,
     },
-  },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 1.25,
+    spaceBetween: 24,
+
+    breakpoints: {
+      // ウィンドウサイズが767px以下
+      767: {
+        slidesPerView: 1.25,
+        spaceBetween: 24,
+      },
+
+      768: {
+        slidesPerView: 3.5,
+        spaceBetween: 41,
+      },
+    },
+  });
+
 });
