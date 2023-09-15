0c86281ef5a99a741ea1be1442a36af918a15644
hiropino1515 <hiropino1515@gmail.com>
2023-09-15 12:45:29 +0900

    14 commit

--- src/js/script.js
+++ src/js/script.js
@@ -15,6 +15,21 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     }
   });
 
+
+  var footer = $('.footer').innerHeight(); // footerの高さを取得
+  window.onscroll = function () {
+    var point = window.pageYOffset; // 現在のスクロール地点
+    var docHeight = $(document).height(); // ドキュメントの高さ
+    var dispHeight = $(window).height(); // 表示領域の高さ
+
+    if(point > docHeight-dispHeight-footer){ // スクロール地点>ドキュメントの高さ-表示領域-footerの高さ
+      $('.to-top').addClass('is-hidden'); //footerより下にスクロールしたらis-hiddenを追加
+    }else{
+        $('.to-top').removeClass('is-hidden'); //footerより上にスクロールしたらis-hiddenを削除
+    }
+  };
+
+
   // ドロワーメニュー
   $(".js-hamburger").click(function () {
     if ($(".js-hamburger").hasClass("is-active")) {
